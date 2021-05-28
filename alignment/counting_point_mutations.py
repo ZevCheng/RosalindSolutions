154fa6c64ca6f2e68e7271b8c326ce5cacafa369
ZevCheng <73809243+ZevCheng@users.noreply.github.com>
2021-05-28 14:40:12 +0800

    汉明距离
    http://rosalind.info/problems/hamm/

--- alignment/counting_point_mutations.py
+++ alignment/counting_point_mutations.py
@@ -9,9 +9,7 @@ Created on Mon Mar 29 21:47:31 2021
 def hamming_distance(s, t):
     return sum(1 for i, j in zip(s, t) if i != j)
 
-
 if __name__ == '__main__':
     with open('rosalind_hamm.txt', 'r', encoding='utf-8') as handle:
         s_, t_ = handle.readlines()
     print(hamming_distance(s_, t_))
-
